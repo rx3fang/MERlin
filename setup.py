5ee630fbc09b632d6bfc1d672c4de8949609d5cc
Rongxin Fang <r3fang@ucsd.edu>
2022-04-07 16:54:51 +0000

    update version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [line.rstrip() for line in open(
 
 setuptools.setup(
     name="merlin",
-    version="0.3.1",
+    version="0.3.2",
     description="MERFISH decoding software",
     author="George Emanuel",
     author_email="emanuega0@gmail.com",
