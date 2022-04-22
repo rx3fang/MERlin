84c5f3a5aafa85aeaecfd37b2444adb4f5c744e4
Rongxin Fang <r3fang@ucsd.edu>
2022-04-22 19:38:51 +0000

    add ImageEnhanceProcess

--- merlin/analysis/preprocess.py
+++ merlin/analysis/preprocess.py
@@ -8,6 +8,11 @@ from merlin.util import aberration
 from merlin.util import imagefilters
 from merlin.data import codebook
 
+from csbdeep.utils import axes_dict, plot_some, plot_history
+from csbdeep.utils.tf import limit_gpu_memory
+from csbdeep.io import load_training_data
+from csbdeep.models import Config, CARE
+
 class Preprocess(analysistask.ParallelAnalysisTask):
 
     """
@@ -41,7 +46,8 @@ class DeconvolutionPreprocess(Preprocess):
         super().__init__(dataSet, parameters, analysisName)
 
         if 'highpass_sigma' not in self.parameters:
-            self.parameters['highpass_sigma'] = 3
+            self.parameters['highpass_sigma'] = 2
+        # disable deconvolution
         if 'decon_sigma' not in self.parameters:
             self.parameters['decon_sigma'] = -1
         if 'decon_filter_size' not in self.parameters:
@@ -169,25 +175,92 @@ class DeconvolutionPreprocess(Preprocess):
         
         self._save_pixel_histogram(pixelHistogram, fragmentIndex)
 
-class DeconvolutionPreprocessGuo(DeconvolutionPreprocess):
+class ImageEnhanceProcess(Preprocess):
 
     def __init__(self, dataSet, parameters=None, analysisName=None):
         super().__init__(dataSet, parameters, analysisName)
 
-        # Check for 'decon_iterations' in parameters instead of
-        # self.parameters as 'decon_iterations' is added to
-        # self.parameters by the super-class with a default value
-        # of 20, but we want the default value to be 2.
-        if 'decon_iterations' not in parameters:
-            self.parameters['decon_iterations'] = 2
+        if 'highpass_sigma' not in self.parameters:
+            self.parameters['highpass_sigma'] = 3
 
-        self._deconIterations = self.parameters['decon_iterations']
+        self._highPassSigma = self.parameters['highpass_sigma']
+        self._modelName = self.parameters['model_name']
+        self._modelBaseDir = self.parameters['model_base_dir']
 
-    def _preprocess_image(self, inputImage: np.ndarray) -> np.ndarray:
-        deconFilterSize = self.parameters['decon_filter_size']
+        self.warpTask = self.dataSet.load_analysis_task(
+            self.parameters['warp_task'])
 
-        filteredImage = self._high_pass_filter(inputImage)
-        deconvolvedImage = deconvolve.deconvolve_lucyrichardson_guo(
-            filteredImage, deconFilterSize, self._deconSigma,
-            self._deconIterations).astype(np.uint16)
-        return deconvolvedImage
+    def fragment_count(self):
+        return len(self.dataSet.get_fovs())
+
+    def get_estimated_memory(self):
+        return 2048
+
+    def get_estimated_time(self):
+        return 5
+
+    def get_dependencies(self):
+        return [self.parameters['warp_task']]
+
+    def get_codebook(self) -> codebook.Codebook:
+        return self.dataSet.get_codebook(self.parameters['codebook_index'])
+    
+    def get_processed_image_set(
+            self, fov, zIndex: int = None,
+            chromaticCorrector: aberration.ChromaticCorrector = None
+    ) -> np.ndarray:
+        if zIndex is None:
+            return np.array([[self.get_processed_image(
+                fov, self.dataSet.get_data_organization()
+                    .get_data_channel_for_bit(b), zIndex, chromaticCorrector)
+                for zIndex in range(len(self.dataSet.get_z_positions()))]
+                for b in self.get_codebook().get_bit_names()])
+        else:
+            return np.array([self.get_processed_image(
+                fov, self.dataSet.get_data_organization()
+                    .get_data_channel_for_bit(b), zIndex, chromaticCorrector)
+                    for b in self.get_codebook().get_bit_names()])
+
+    def get_processed_image(
+            self, fov: int, dataChannel: int, zIndex: int,
+            chromaticCorrector: aberration.ChromaticCorrector = None
+    ) -> np.ndarray:
+        inputImage = self.warpTask.get_aligned_image(fov, dataChannel, zIndex,
+                                                     chromaticCorrector)
+        
+        return self._preprocess_image(inputImage, imageColor)
+    
+    def _preprocess_image(
+            self, inputImage: np.ndarray, imageColor: str
+    ) -> np.ndarray:
+        # adjust the image illumination using dark stage
+        # and flat field image
+        
+        imageDark = self.dataSet.illuminationCorrections[imageColor]["dark"]
+        imageFlat = self.dataSet.illuminationCorrections[imageColor]["flat"]
+        correctedImage = (inputImage - imageDark) / imageFlat
+
+        # make sure values of the corrected images are not negative
+        correctedImage = np.clip(correctedImage, 
+            a_min=0, a_max=correctedImage.max())
+
+        # high pass filter to remove background
+        filteredImage = self._high_pass_filter(correctedImage)
+        
+        imageSize = filteredImage.shape
+        # deconvolution
+        model = CARE(config = None, name = self._modelName, basedir=self._modelBaseDir)
+        predictedImage = model.keras_model.predict(
+            filteredImage.reshape(imageSize[0], imageSize[1], 
+                                  imageSize[2], 1))
+        predictedImage = predictedImage.reshape(imageSize[0], 
+                                                imageSize[1], 
+                                                imageSize[2])
+        return np.where(predictedImage < 0, 0, predictedImage)
+
+    def _high_pass_filter(self, inputImage: np.ndarray) -> np.ndarray:
+        highPassFilterSize = int(2 * np.ceil(2 * self._highPassSigma) + 1)
+        hpImage = imagefilters.high_pass_filter(inputImage,
+                                                highPassFilterSize,
+                                                self._highPassSigma)
+        return hpImage.astype(np.float)
