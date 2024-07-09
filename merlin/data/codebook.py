2943e6acf7209389dc1935f66d05b0ccc6fb10bb
rx3fang <rx3fang@gmail.com>
2024-07-09 17:17:02 -0400

    remove SyntaxWarning: "is not" with a literal. Did you mean "!="?

--- merlin/data/codebook.py
+++ merlin/data/codebook.py
@@ -9,7 +9,7 @@ import merlin
 
 
 def _parse_barcode_from_string(inputString):
-    return np.array([int(x) for x in inputString if x is not ' '])
+    return np.array([int(x) for x in inputString if x != ' '])
 
 
 class Codebook(object):
